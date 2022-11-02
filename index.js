8598ad2df00585bc3f15da582329141215339f3d
Michael Marin <michael_6-c@hotmail.com>
2022-11-01 21:22:52 -0300

    commit 01/11/2022

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const menu = document.querySelector('.menu-lateral')
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo')
 })
+
+const botaoNotificacao = document.querySelector('.cabecalho_notificacao')
+const notificacao = document.querySelector('.notificacao-lateral')
+
+botaoNotificacao.addEventListener('click', () => {
+    notificacao.classList.toggle('notificacao-lateral--ativo')
+})
